81ead682e6c26a50a948fc9f4cc609d3cf03738a
jhkim <user@users-iMac.local>
2017-09-23 14:38:06 +0900

    add

--- mining/terabit_mine.sh
+++ mining/terabit_mine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 MINER=../minerd
-$MINER  -q -t 1 -a scrypt --url stratum+tcp://192.168.0.10:13008 --userpass TYeqG4sfBH8jAJHDQq8UHg6dW431oeFHh2:x
+$MINER  -q -t 1 -a scrypt --url stratum+tcp://127.0.0.1:13008 --userpass TYeqG4sfBH8jAJHDQq8UHg6dW431oeFHh2:x
---
+++ mining/ventas_mine.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+MINER=../minerd
+$MINER  -q -t 1 -a scrypt --url stratum+tcp://127.0.0.1:18008 --userpass VGWsQn23oasJqRjjhV5LuhPqbanSkqkLoR:x
